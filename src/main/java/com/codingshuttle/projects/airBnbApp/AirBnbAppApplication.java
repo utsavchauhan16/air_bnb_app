1d1c887cad33ec0bfbce0e839afadb1ea6d24fa4
Utsav Chauhan <utsavrajput16002@gmail.com>
2025-05-04 21:45:04 +0530

    first blood!

---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,16 @@
+package com.codingshuttle.projects.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+		System.out.println("running successfully");
+	}
+
+}
